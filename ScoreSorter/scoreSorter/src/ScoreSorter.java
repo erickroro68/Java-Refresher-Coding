d868c2b060c7c79ad4753a559d09845a5ce74aba
erickroro68 <36974044+erickroro68@users.noreply.github.com>
2026-05-20 10:30:59 -0500

    Starting ScoreSorter: this is scoreSorter chllenge on codecademy

---
+++ ScoreSorter/scoreSorter/src/ScoreSorter.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ScoreSorter {
+  public static void main(String[] args) {
+    int[] answer = scoreSorter(new int[]{1, 2, 3, 9999, 13}, 10000);
+    for (int i = 0; i < answer.length; i++) {
+      System.out.println(answer[i]);
+    }
+  }
+
+  public static int[] scoreSorter(int[] array, int topScore) {
+    // Write your code here
+  }
+}
