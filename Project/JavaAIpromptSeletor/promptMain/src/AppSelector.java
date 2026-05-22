331c5b64b9107dfcb13c8937d1909ac0b14f1881
erickroro68 <36974044+erickroro68@users.noreply.github.com>
2026-05-22 12:37:33 -0500

    Adding a new Folder for java projects that are in the realm of intermediate

---
+++ Project/JavaAIpromptSeletor/promptMain/src/AppSelector.java
@@ -0,0 +1,9 @@
+public class AppSelector {
+    public static void main(String[] args) {
+        System.out.println("===========================" );
+        System.out.println("    PROMPT SELECT MENU     " );
+        System.out.println("===========================" );
+        System.out.println(" 1) View Prompts" );
+        System.out.println(" 2) Quit CLI" );
+    }
+}
