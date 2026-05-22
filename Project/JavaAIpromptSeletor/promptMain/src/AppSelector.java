3d577e6b5064272ba6fd68412c3227bdcc1d71fa
erickroro68 <36974044+erickroro68@users.noreply.github.com>
2026-05-22 12:43:10 -0500

    Second Commit: forogot to save file with my updated code so far

--- Project/JavaAIpromptSeletor/promptMain/src/AppSelector.java
+++ Project/JavaAIpromptSeletor/promptMain/src/AppSelector.java
@@ -1,9 +1,31 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
 public class AppSelector {
     public static void main(String[] args) {
+
+        String promptFolderPath =  "X:\\CODE\\Visual STUDIO\\Customer-MasterPrompt_Launch_Picker\\UsefulPrompts\\private_prompts";
+        File Folder = new File(promptFolderPath);
+        File[] files = Folder.listFiles();
+
+        Scanner userInput = new Scanner(System.in);
+        
         System.out.println("===========================" );
         System.out.println("    PROMPT SELECT MENU     " );
         System.out.println("===========================" );
         System.out.println(" 1) View Prompts" );
         System.out.println(" 2) Quit CLI" );
+    
+        switch (userInput) {
+            case 1 :
+                System.out.println("ALL AVAILALE PROMPTS");
+
+                break;
+            default:
+                throw new AssertionError();
+        }
+
     }
 }
