dcf72f99d6edc8221b7ed7819703d697feedb7d6
erickroro68 <36974044+erickroro68@users.noreply.github.com>
2026-05-22 13:52:13 -0500

    3rd commit: work on later just fxed switch and other statementss

--- Project/JavaAIpromptSeletor/promptMain/src/AppSelector.java
+++ Project/JavaAIpromptSeletor/promptMain/src/AppSelector.java
@@ -1,27 +1,33 @@
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Scanner;
 public class AppSelector {
     public static void main(String[] args) {
 
-        String promptFolderPath =  "X:\\CODE\\Visual STUDIO\\Customer-MasterPrompt_Launch_Picker\\UsefulPrompts\\private_prompts";
-        File Folder = new File(promptFolderPath);
+        String promptsFolderPath =  "X:\\CODE\\Visual STUDIO\\Customer-MasterPrompt_Launch_Picker\\UsefulPrompts\\private_prompts";
+        File Folder = new File(promptsFolderPath);
         File[] files = Folder.listFiles();
 
         Scanner userInput = new Scanner(System.in);
-        
+
         System.out.println("===========================" );
         System.out.println("    PROMPT SELECT MENU     " );
         System.out.println("===========================" );
         System.out.println(" 1) View Prompts" );
         System.out.println(" 2) Quit CLI" );
-    
-        switch (userInput) {
+        
+
+        //adding and or changing so Scanner has to be a Integer input / 'userInput'
+        int usersChoice = userInput.nextInt();
+        switch (usersChoice) {
             case 1 :
                 System.out.println("ALL AVAILALE PROMPTS");
+                System.out.println("======================");
 
+                for (int i = 0; i < files.length; i++) {
+                    if (files[i].isFile() && files[i].getName().endsWith(".txt")) {
+                    System.out.println((i + 1) + ". " + files[i].getName());
+            }
+        }
                 break;
             default:
                 throw new AssertionError();
