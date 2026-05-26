7a0cb59b99981e509be8f20a5649254f43ac1801
erickroro68 <36974044+erickroro68@users.noreply.github.com>
2026-05-25 22:13:09 -0500

    added comments to our java solution to be more detailed

--- Leetcode_Folder_Problems/Arrays/TreasureShelf.java
+++ Leetcode_Folder_Problems/Arrays/TreasureShelf.java
@@ -1,7 +1,8 @@
 public class TreasureShelf {
 
     public static void main(String[] args) {
-
+        //making a String array called 'shelf'
+        // our String array called 'shelf' contains 5 String values
         String[] shelf = {
             "GEM",
             "EMPTY",
@@ -10,10 +11,12 @@ public class TreasureShelf {
             "MAP"
         };
 
+        //making a integer called 'coinsAdded' that will  contain
+        //  the count of the amount of times we see the String literal called "EMPTY" 
         int coinsAdded = fillShelf(shelf);
-
+        //shows us the integer of the amount of times we see the String "EMPTY"
         System.out.println("COINS ADDED: " + coinsAdded);
-
+        
         System.out.println("\nUpdated Shelf:");
 
         // PRINT UPDATED ARRAY
@@ -28,22 +31,29 @@ public class TreasureShelf {
     }
 
     public static int fillShelf(String[] shelf) {
-
+        //making a integer named count since we havve to return a Integer at the end of method
         int count = 0;
 
         // LOOP THROUGH ARRAY
-
+        // we are looping through each String using method .length 
+        // which loops through each String in our String array called 'shelf'
         for (int i = 0; i < shelf.length; i++) {
 
             // IF CURRENT VALUE IS "EMPTY"
+            // checking each index value using the method .equals 
+            // IF the current index value is equal to the literal String named 'EMPTY'
+            // then goes to the logic we have inside the If statement
             // TODO:
             if (shelf[i].equals("EMPTY")) {
+                //we replace OR swap the index VALUE IF it equals "EMPTY"
+                // with 'COIN' instead and adding 1 or incrementing our count value by 1
                 shelf[i] = "COIN";
                 count++;
             }
             
         
         }
+        //returning  the Integer of the amount of times we swaped the index VALUE
         return count;
     }
 }
