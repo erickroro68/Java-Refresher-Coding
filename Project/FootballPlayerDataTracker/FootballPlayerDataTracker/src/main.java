71b8bab38bfb2fcd06124844b79cd8006314d1c2
erickroro68 <36974044+erickroro68@users.noreply.github.com>
2026-06-16 14:24:47 -0500

    Added test cases for the classes i made

--- Project/FootballPlayerDataTracker/FootballPlayerDataTracker/src/main.java
+++ Project/FootballPlayerDataTracker/FootballPlayerDataTracker/src/main.java
@@ -4,21 +4,30 @@ public class main {
         Player p1 = new Player("Erick Ro", "OL", "Vikings", 220, 6.1);
         Player p2 = new Player("Justin Jefferson", "WR", "Vikings", 195, 6.1);
         Player p3 = new Player("Patrick Mahomes", "QB", "Chiefs", 225, 6.2);
-
-        footballTracker tracker = new footballTracker();
-        LeaderBoard leaders = new LeaderBoard();
+        Player p4 = new Player("Ben Rummer", "RB", "Baltimore Ravens", 198, 5.8);
+        FootballTracker tracker = new FootballTracker();
+        LeaderBoardServices leaders = new LeaderBoardServices();
+        Simulations sim = new Simulations();
         tracker.addPlayer(p1);
         tracker.addPlayer(p2);
         tracker.addPlayer(p3);
+        tracker.addPlayer(p4);
+
+        p1.addTouchdown();
+        p1.addRunningYards(12);
+
+        p2.addTouchdown();
+        p2.addRunningYards(12);
+        p2.addPassingYards(69);
+
+        p3.addTouchdown();
+        p3.addRunningYards(12);
+        p3.addPassingYards(123);
 
         tracker.viewPlayers();
 
         tracker.viewPlayerStats(2);
 
-            foundPlayer.printAllStats();
-        } else {
-            System.out.println("Player Not Found");
-        }
-
+        sim.printWeekSimulation(tracker.getPlayerList());
     }
 }
